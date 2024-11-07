fd711bf4a181cd95a14b147798b318d42a26f278
wowseok <wowseok2774@gmail.com>
2024-11-07 17:13:38 +0900

    feat:  String 데이터를 가공하여 매핑 ✔️
    - 파일 입출력을 통해 String으로 저장한 데이터를 줄마다 저장한다

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # java-convenience-store-precourse
 
 src/main/resources/products.md 파일을 파일 입출력을 통해 불러온다. ✔️
+파일 입출력을 통해 String으로 저장한 데이터를 줄마다 저장한다 ✔️
+product 클래스를 만들어 이름, 가격, 프로모션을 가질 수 있게 한다.
 src/main/resources/promotions.md 파일을 파일 입출력을 통해 불러온다.
 구매할 상품과 수량을 입력 받는다. ✔️
 상품명, 수량은 하이픈(-)으로 구분한다.
---
+++ src/main/java/store/global/utility/StringParser.java
@@ -0,0 +1,45 @@
+package store.global.utility;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class StringParser {
+    public static List<Map<String, String>> parseFileContent(String fileContent) {
+        String[] lines = fileContent.split("\n");
+        String[] headers = parseHeaders(lines[0]);
+        return parseData(lines, headers);
+    }
+
+    private static String[] parseHeaders(String headerLine) {
+        return headerLine.split(",");
+    }
+
+    private static List<Map<String, String>> parseData(String[] lines, String[] headers) {
+        List<Map<String, String>> parsedDataList = new ArrayList<>();
+        for (int i = 1; i < lines.length; i++) {
+            Map<String, String> parsedData = mapLineToData(lines[i], headers);
+            parsedDataList.add(parsedData);
+        }
+        return parsedDataList;
+    }
+
+    private static Map<String, String> mapLineToData(String line, String[] headers) {
+
+        String[] values = line.split(",");
+        Map<String, String> parsedData = new LinkedHashMap<>();
+        for (int j = 0; j < headers.length; j++) {
+            parsedData.put(headers[j], getValue(values, j));
+        }
+
+        return parsedData;
+    }
+
+    private static String getValue(String[] values, int index) {
+        if (index < values.length) {
+            return values[index];
+        }
+        return null;
+    }
+}
